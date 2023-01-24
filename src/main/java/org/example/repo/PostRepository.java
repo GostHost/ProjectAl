793ff78507157d45d31b879ee9d4a45e057f578a
GostHost <calmghost@yandex.ru>
2023-01-24 17:51:05 +0400

    Initial commit

---
+++ src/main/java/org/example/repo/PostRepository.java
@@ -0,0 +1,7 @@
+package org.example.repo;
+
+import org.example.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository <Post, Long> {
+}
